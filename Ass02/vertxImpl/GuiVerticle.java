6a3d6ac3a8682d7b08d8e93f2085295399db02cd
Manuele Pasini <manuele.pasini@studio.unibo.it>
2020-05-06 11:12:27 +0200

    updated with new GUI

--- Ass02/vertxImpl/GuiVerticle.java
+++ Ass02/vertxImpl/GuiVerticle.java
@@ -1,11 +1,13 @@
 package vertxImpl;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.eventbus.EventBus;
+
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
@@ -13,32 +15,39 @@ public class GuiVerticle extends AbstractVerticle {
 	private Gui view;
 	private EventBus eb;
 	private Graph graph;
+	private List<NodeTuple> nodes;
+	private int nodeIndex = 0;
+	private int edgeIndex = 0;
 	private static final int DEPT = 0;
 	
 	@Override
 	public void start(final Future<Void> startFuture) {
 		eb = vertx.eventBus();
 		graph = new SingleGraph("grafo");
+		view = new Gui(1024, 768,eb,graph);
+		nodes = new LinkedList<NodeTuple>();
+		view.display();
 		eb.consumer("init", message -> {
 			 String[] f = message.body().toString().split(":");
-			 String word = f[0]; 
+			 String word= f[0]; 
 			 int dept = Integer.parseInt(f[1]);
 			 vertx.deployVerticle(new MyVerticle(DEPT,dept,Arrays.asList(word)));
 		});
 		eb.consumer("updateView", message -> {
+			System.out.println("RECEIVED");
 			//view.updateView((List<String>)message.body());
 			DataHolder c = (DataHolder) message.body();
 			List<NodeTuple> newWords = c.getData();
+			System.out.println(newWords.size());
 			for(NodeTuple t : newWords) {
 				System.out.println(t.getValue());
 			}
-			view.updateView(newWords);
+			nodes.addAll(newWords);
+			updateView(newWords);
 		});
 		eb.consumer("stop", message -> {
 			vertx.close();
 		});
-		view = new Gui(500,180,eb);
-		view.display();
 	}
 	
 	@Override
@@ -46,32 +55,26 @@ public class GuiVerticle extends AbstractVerticle {
 		System.out.println("MyVerticle stopped!");
 	}
 	
+	private void updateView(final List<NodeTuple> values) {
+		for(NodeTuple node : values) {		
+			String value = node.getValue();
+			String father = node.getFather();
+			synchronized (graph) {
+				try {
+					if(!nodeExists(value)) {
+						graph.addNode(value);
+						graph.getNode(value).addAttribute("ui.label", graph.getNode(value).getId());
+						System.out.println("NNOODDII: "+graph.getNodeCount());
+						graph.addEdge(father+value,father,value);		
+					}else {	
+						graph.addEdge(father+value,father,value);
+					}
+				} catch(Exception e) {}
+			}
+		}
+	}
+	
 	private boolean nodeExists (String title) {
 		return graph.getNode(title) != null;
-	}
-	
-	private boolean edgeExists (String title) {
-		return graph.getEdge(title) != null;
-	}
-	
-	private void addNode (String title) {
-		synchronized (graph) {
-			try {
-				if(!nodeExists(title)) {
-					graph.addNode(title);
-					graph.getNode(title).addAttribute("ui.label", graph.getNode(title).getId());
-				}
-			} catch(Exception e) {}
-		}
-	}
-	
-	private void addEdge (String title, String elem1, String elem2) {
-		synchronized (graph) {
-			try {
-				if(!edgeExists(title)) {
-					graph.addEdge(title, elem1, elem2);
-				}
-			} catch (Exception e) {}
-		}
 	}
 }
--- Ass02/vertxImpl/MyVerticle.java
+++ Ass02/vertxImpl/MyVerticle.java
@@ -18,12 +18,14 @@ import io.vertx.rxjava.core.CompositeFuture;
 
 public class MyVerticle extends AbstractVerticle {
 	private static final String WIKI = "hhttps://it.wikipedia.org/w/api.php";
+	private static final int ROOT = 0;
 	private WebClient client;
 	private List<NodeTuple> words;
 	private EventBus eb;
 	private int dept;
 	private int maxDept;
 	private List<String> assigned;
+	private List<String> toAssign;
 	private JsonArray links;
 	
 	public MyVerticle(int dept, int maxDept, final List<String> voices) {
@@ -35,6 +37,14 @@ public class MyVerticle extends AbstractVerticle {
 	@Override
 	public void start(Future<Void> startFuture) {
 		System.out.println("I've been deployed!");
+	    this.words = new LinkedList<NodeTuple>();
+	    this.toAssign = new LinkedList<String>();
+		eb = vertx.eventBus();
+	    if(dept == ROOT) {
+	    	ArrayList<NodeTuple> tmp = new ArrayList<NodeTuple>();
+	    	tmp.add(new NodeTuple("root",assigned.get(ROOT)));
+	    	eb.send("updateView", new DataHolder(tmp));
+	    }
 		Future<Void> steps;
 		if(dept < maxDept) {
 		     client = WebClient.create(vertx,
@@ -46,11 +56,9 @@ public class MyVerticle extends AbstractVerticle {
 		                    .setDefaultHost("www.wikipedia.org")
 	
 		    );
-		    this.words = new LinkedList<NodeTuple>();
 			System.out.println("MyVerticle started!");
-			eb = vertx.eventBus();
+			//eb.send("updateView", new DataHolder(new NodeTuple(ROOT,assigned.get(0)));
 			steps =  compute(assigned)
-					.compose(v -> updateWordList("0", links))
 					.compose(v -> send())
 					.onComplete(t -> {
 						vertx.deployVerticle(new MyVerticle(dept,maxDept,assigned));
@@ -83,8 +91,7 @@ public class MyVerticle extends AbstractVerticle {
 		        .send(ar -> {
 		            if (ar.succeeded()) {
 		            	System.out.println("CERCANDO..");
-		                promises.add(getWords(ar.result()));
-		    			System.out.println(promises.size());
+		                promises.add(getWords(ar.result(),father));
 		            } else {
 		                ar.cause().printStackTrace();
 		            }
@@ -102,24 +109,30 @@ public class MyVerticle extends AbstractVerticle {
 		return promise.future();
 	}
 	
-	private Future<Void>getWords(HttpResponse<JsonObject> response){
+	private Future<Void>getWords(HttpResponse<JsonObject> response,String father){
 		Promise<Void> promise = Promise.promise();
 		JsonObject body = response.body();
-		links.add(body.getJsonObject("parse").getJsonArray("links"));
-		promise.complete();
+		/**
+		 * Check if links has to be assigned or has to be links.add
+		 */
+		links = body.getJsonObject("parse").getJsonArray("links");
+		System.out.println(links);
+		//assigned = new ArrayList<String>();
+		//words = new ArrayList<NodeTuple>();
+		for (int i = 0; i < links.size(); i++) {
+			String value = links.getJsonObject(i).getString("*");
+			if (links.getJsonObject(i).getLong("ns") == 0) {
+					words.add(new NodeTuple(father,value));
+			}
+			toAssign.add(value);
+			if(i == links.size()-1) {
+				promise.complete();
+			}
+		}
 		return promise.future();
 	}
-	private Future<Void> send(){
-		Promise<Void>promise = Promise.promise();
-		System.out.println("Sending");
-		eb.send("updateView", new DataHolder(words));
-		promise.complete();
-		System.out.println("SENT");
-		return promise.future();
-	}
-	
 
-	private Future<Void> updateWordList(final String father,final JsonArray links) {
+	/*private Future<Void> updateWordList(final String father,final JsonArray links) {
 		Promise<Void> promise = Promise.promise();
 		System.out.println("Updating lists");
 		assigned = new ArrayList<String>();
@@ -138,6 +151,16 @@ public class MyVerticle extends AbstractVerticle {
 		promise.complete();
 		System.out.println("Updated");
 		return promise.future();
+	}*/
+	
+	private Future<Void> send(){
+		Promise<Void>promise = Promise.promise();
+		System.out.println("Sending");
+		eb.send("updateView", new DataHolder(words));
+		promise.complete();
+		System.out.println("SENT");
+		return promise.future();
 	}
+	
 	
 }
