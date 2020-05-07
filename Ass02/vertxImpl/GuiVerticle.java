4675db137784f62786af44196e1a988dc1a4d8d1
Manuele Pasini <manuele.pasini@studio.unibo.it>
2020-05-07 14:58:54 +0200

    Implementation completed, needs refactoring

--- Ass02/vertxImpl/GuiVerticle.java
+++ Ass02/vertxImpl/GuiVerticle.java
@@ -1,8 +1,6 @@
 package vertxImpl;
 
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
@@ -15,70 +13,82 @@ public class GuiVerticle extends AbstractVerticle {
 	private Gui view;
 	private EventBus eb;
 	private Graph graph;
-	private List<NodeTuple> nodes;
-	private int nodeIndex = 0;
-	private int edgeIndex = 0;
 	private static final int DEPT = 0;
-	long initialMillis;
-	long finalMillis;
+	private long initialMillis;
+	private long finalMillis;
+	private int count;
+	private int instancies;
+	private int maxDept;
 	
+	public GuiVerticle(final int instancies) {
+		this.instancies = instancies;
+	}
 	@Override
 	public void start(final Future<Void> startFuture) {
+		try {
+			super.start(startFuture);
+		} catch (Exception e) {
+		}
+		count = 0;
 		eb = vertx.eventBus();
 		graph = new SingleGraph("grafo");
-		view = new Gui(1024, 768,eb,graph);
-		nodes = new LinkedList<NodeTuple>();
+		view = new Gui(1024, 768, eb, graph);
 		view.display();
 		eb.consumer("init", message -> {
-			 String[] f = message.body().toString().split(":");
-			 LinkedList<String>words = new LinkedList<String>();
-			 words.add(f[0]);
-			 int dept = Integer.parseInt(f[1]);
-			 initialMillis = System.currentTimeMillis();
-			 vertx.deployVerticle(new MyVerticle(DEPT,dept,words));
+				String[] f = message.body().toString().split(":");
+				LinkedList<String> words = new LinkedList<String>();
+				words.add(f[0]);
+				maxDept = Integer.parseInt(f[1]);
+				initialMillis = System.currentTimeMillis();
+				vertx.deployVerticle(new MyVerticle(instancies,DEPT, maxDept, words));
 		});
 		eb.consumer("updateView", message -> {
-			//System.out.println("RECEIVED");
-			//view.updateView((List<String>)message.body());
+			// System.out.println("RECEIVED");
+			// view.updateView((List<String>)message.body());
 			DataHolder c = (DataHolder) message.body();
 			NodeTuple newWords = c.getData();
-
-			nodes.add(newWords);
 			updateView(newWords);
 		});
 		eb.consumer("stop", message -> {
+			System.out.println("RECEIVED A STOP; WAITING FOR OTHERS");
 			finalMillis = System.currentTimeMillis();
-			long diff = finalMillis-initialMillis;
-			System.out.println("STOP, TIME:"+ diff);
-			vertx.deploymentIDs().forEach(vertx::undeploy);
-			view.updateLabel();
-			//vertx.close();
+			long diff = finalMillis - initialMillis;
+			count++;
+			if (count == Math.pow(instancies, maxDept-1)) {
+				System.out.println("EVERYONE STOPPED, TIME:" + diff);
+				//vertx.deploymentIDs().forEach(vertx::undeploy);
+				view.updateLabel();
+				vertx.close();
+			}
+
 		});
 	}
-	
+
 	@Override
 	public void stop(final Future<Void> stopFuture) throws Exception {
-		System.out.println("MyVerticle stopped!");
+		super.stop(stopFuture);
+		System.out.println("MyGUIVerticle stopped!");
 	}
-	
+
 	private void updateView(final NodeTuple node) {
-			String value = node.getValue();
-			String father = node.getFather();
-			synchronized (graph) {
-				try {
-					if(!nodeExists(value)) {
-						graph.addNode(value);
-						graph.getNode(value).addAttribute("ui.label", graph.getNode(value).getId());
-						//System.out.println("NNOODDII: "+graph.getNodeCount());
-						graph.addEdge(father+value,father,value);		
-					}else {	
-						graph.addEdge(father+value,father,value);
-					}
-				} catch(Exception e) {}
+		String value = node.getValue();
+		String father = node.getFather();
+		synchronized (graph) {
+			try {
+				if (!nodeExists(value)) {
+					graph.addNode(value);
+					graph.getNode(value).addAttribute("ui.label", graph.getNode(value).getId());
+					// System.out.println("NNOODDII: "+graph.getNodeCount());
+					graph.addEdge(father + value, father, value);
+				} else {
+					graph.addEdge(father + value, father, value);
+				}
+			} catch (Exception e) {
 			}
+		}
 	}
-	
-	private boolean nodeExists (String title) {
+
+	private boolean nodeExists(String title) {
 		return graph.getNode(title) != null;
 	}
 }
